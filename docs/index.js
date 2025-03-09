f3cae5149f6437cfc21babd215a5fe6108576a5d
Cow-Mangler-5000 <nurhafizrahimi9@gmail.com>
2025-03-09 17:56:39 +0800

    Deploy to GitHub Pages

---
+++ docs/index.js
@@ -0,0 +1,13 @@
+import init, { fibonacci } from "./pkg/fibcalc.js";
+
+const calculate = document.getElementById("calculate")
+const input = document.getElementById("inp")
+const result = document.getElementById("result")
+
+init().then(() => {
+    calculate.addEventListener("click", async () => {
+        let x = input.value
+        let y = BigInt(x)
+        result.textContent = fibonacci(y);
+    })
+})
